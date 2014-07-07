e22f828617833b0cdf042eb1d5e8eff3d89b84ff
Adam Balchunas <adam.balchunas@digitas.com>
2014-07-07 14:51:11 -0400

    Refactored Quicksort

--- src/Quicksort.js
+++ src/Quicksort.js
@@ -1,21 +1,20 @@
-algo.namespace('quicksort');
+algo.namespace('Quicksort');
 
-algo.quicksort = (function() {
-  function sort(a) {
-    var left = [], right = [], pivot = a[0];
+algo.Quicksort = function() {
+};
+
+algo.Quicksort.prototype.sort = function(a) {
+  var left = [], 
+      right = [], 
+      pivot = a[0];
     
-    if (a.length === 0) {
+  if (a.length === 0) {
       return [];
-    }
-
-    for (var i = 1; i < a.length; i++) {
-      a[i] < pivot ? left.push(a[i]) : right.push(a[i]);
-    }
-
-    return sort(left).concat(pivot, sort(right));
   }
 
-  return {
-    sort: sort
-  };
-}());
+  for (var i = 1; i < a.length; i++) {
+    a[i] < pivot ? left.push(a[i]) : right.push(a[i]);
+  }
+
+  return this.sort(left).concat(pivot, this.sort(right));
+};
